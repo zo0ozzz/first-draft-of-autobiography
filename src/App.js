91cee9a904bedbcccb11a0e1b5b3ebb56546bb28
[SW4/2반] 정웅비 <zo0qzzz@gmail.com>
2023-05-18 06:54:21 +0900

    시멘틱 & grid 맹연습

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app grid-container">
+      <header className="grid-item-header">
+        <Header></Header>
       </header>
+      <main className="grid-item-main main-grid-container">
+        <div className="main-grid-item-section-tag">
+          <section className="main-section-tag">
+            <div className="container-tag">
+              <div className="tag">태그1</div>
+              <div className="tag">태그2</div>
+              <div className="tag">태그3</div>
+              <div className="tag">태그4</div>
+              <div className="tag">태그5</div>
+              <div className="tag">태그6</div>
+            </div>
+          </section>
+        </div>
+        <div className="main-grid-item-section-note">
+          <section className="main-section-note">
+            <div className="container-note">
+              <div className="note">노트1</div>
+              <div className="note">노트2</div>
+              <div className="note">노트3</div>
+              <div className="note">노트4</div>
+              <div className="note">노트5</div>
+              <div className="note">노트6</div>
+            </div>
+          </section>
+        </div>
+      </main>
+      <footer className="grid-item-footer">푸터</footer>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <header>헤더</header>
+    </>
   );
 }
 
