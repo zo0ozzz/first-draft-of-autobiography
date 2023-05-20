c073ad88fb8e33df2c45707fec53c678e2ee278e
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-05-21 08:31:06 +0900

    desk

--- src/App.js
+++ src/App.js
@@ -1,74 +1,59 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import note from "./noteData";
 
 function App() {
   const pageTitle = "초고";
   const [navItem, setNavItem] = useState(["nav1", "nav2", "nav3"]);
   const [tag, setTag] = useState(["태그1", "태그2", "태그3", "태그4"]);
-  const [note, setNote] = useState([
-    {
-      id: 1,
-      title: "팬케이크",
-      content: "과유불급이란 말은 과연 팬케이크에 있어서도 예외가 아닌 것이다.",
-    },
-    {
-      id: 2,
-      title: "팬케이크",
-      content: "과유불급이란 말은 과연 팬케이크에 있어서도 예외가 아닌 것이다.",
-    },
-    {
-      id: 3,
-      title: "팬케이크",
-      content: "과유불급이란 말은 과연 팬케이크에 있어서도 예외가 아닌 것이다.",
-    },
-    {
-      id: 4,
-      title: "팬케이크",
-      content: "과유불급이란 말은 과연 팬케이크에 있어서도 예외가 아닌 것이다.",
-    },
-  ]);
-  const [editable, setEditalbe] = useState("false");
-  const [modal, setModal] = useState(false);
+  const [noteData, setNoteData] = useState(note);
+  // const [switchEditable, setSwitchEditalbe] = useState(false);
+  const [switchModalNote, setSwitchModalNote] = useState(false);
+  const [modalNoteData, setModalNoteData] = useState("");
 
   return (
-    <div className="App grid-container">
-      {modal ? <Modal modal={modal} setModal={setModal}></Modal> : null}
+    <div className="App">
+      {switchModalNote ? (
+        <ModalNote
+          setSwitchModalNote={setSwitchModalNote}
+          noteData={noteData}
+          setNoteData={setNoteData}
+          modalNoteData={modalNoteData}
+          setModalNoteData={setModalNoteData}
+        ></ModalNote>
+      ) : null}
 
-      <Header pageTitle={pageTitle} navItem={navItem}></Header>
-      <div className="grid-item-main">
-        <div className="container-main">
-          <main className="main main-grid-container">
-            <SectionTag tag={tag}></SectionTag>
-            <SectionNote
-              note={note}
-              setModal={setModal}
-              editable={editable}
-            ></SectionNote>
-          </main>
-        </div>
-      </div>
+      <header className="header">
+        <NavBar pageTitle={pageTitle} navItem={navItem}></NavBar>
+      </header>
+      <main className="main">
+        <section className="main-section-tag">
+          <SectionTag tag={tag}></SectionTag>
+        </section>
+        <section className="main-section-note main-section-note-grid-container">
+          <SectionNote
+            noteData={noteData}
+            setSwitchModalNote={setSwitchModalNote}
+            setModalNoteData={setModalNoteData}
+          ></SectionNote>
+        </section>
+      </main>
       <footer id="grid-item-footer footer"></footer>
     </div>
   );
 }
 
-function Header({ pageTitle, navItem }) {
+function NavBar({ pageTitle, navItem }) {
   return (
     <>
-      <div className="grid-item-header">
-        <div className="container-header">
-          <header className="header">
-            <div className="container-logo">
-              <div className="logo">{pageTitle}</div>
-            </div>
-            <div className="container-search-input">
-              <input className="search-input" type="text" placeholder="검색" />
-            </div>
-            <Nav navItem={navItem}></Nav>
-          </header>
-        </div>
+      <div className="container-logo">
+        <div className="logo">{pageTitle}</div>
       </div>
+      <div className="container-search-input">
+        <input className="search-input" type="text" placeholder="검색" />
+      </div>
+      <Nav navItem={navItem}></Nav>
     </>
   );
 }
@@ -102,73 +87,112 @@ function SectionTag({ tag }) {
     );
   });
 
-  return (
-    <div className="main-grid-item-section-tag">
-      <section className="main-section-tag">{arr}</section>
-    </div>
-  );
+  return arr;
 }
 
-function SectionNote({ note, editable, setModal }) {
-  const arr = note.map(({ id, title, content }) => {
+function SectionNote({ noteData, setSwitchModalNote, setModalNoteData }) {
+  const arr = noteData.map(({ id, title, content }) => {
     return (
-      <div key={id} className="main-section-note-grid-item">
-        <div className="container-note">
-          <div
-            className="note"
-            onClick={() => {
-              setModal(true);
-            }}
-          >
-            <div className="container-note-subject">
-              <div className="note-subject" editable={editable}>
-                {title}
-              </div>
-            </div>
-            <div className="container-note-content">
-              <div className="note-content" editable={editable}>
-                {content}
-              </div>
-            </div>
-            {/* <div className="container-note-tool">
-              <div className="container-note-tool-update-button">
-                <button className="note-tool-update-button">수정</button>
-              </div>
-              <div className="container-note-tool-delete-button">
-                <button className="note-tool-update-buttondelete-button">
-                  삭제
-                </button>
-              </div>
-              <div className="container-note-tool-when">
-                <div className="note-tool-when">시간</div>
-              </div>
-            </div> */}
+      <div className="container-note" key={id}>
+        <div
+          className="note"
+          onClick={() => {
+            setSwitchModalNote(true);
+
+            const copy = [...noteData];
+
+            const modalNoteData = copy.find((item) => {
+              return item.id === id;
+            });
+
+            // console.log(modalNoteData);
+
+            setModalNoteData(modalNoteData);
+          }}
+        >
+          <div className="container-note-subject">
+            <div className="note-subject">{title}</div>
+          </div>
+          <div className="container-note-content">
+            <div className="note-content">{content}</div>
           </div>
         </div>
       </div>
     );
   });
-  return (
-    <div className="main-grid-item-section-note">
-      <section className="main-section-note main-section-note-grid-container">
-        {arr}
-      </section>
-    </div>
-  );
+  return arr;
 }
 
-function Modal({ modal, setModal }) {
+function ModalNote({
+  setSwitchModalNote,
+  noteData,
+  setNoteData,
+  setModalNoteData,
+  modalNoteData,
+}) {
+  // const { id, title, content } = ModalNoteData;
   return (
     <>
       <div
-        className="container-modal "
+        className="background-modal-note"
         onClick={(e) => {
           if (e.target == e.currentTarget) {
-            setModal(false);
+            const newNoteData = noteData.find((item) => {
+              return item.id === modalNoteData.id;
+            });
+
+            if (modalNoteData.title !== newNoteData.title) {
+              const copy = [...noteData];
+
+              const index = copy.findIndex((item) => {
+                return item.id === modalNoteData.id;
+              });
+
+              copy[index].title = modalNoteData.title;
+
+              setNoteData(copy);
+            } else {
+              console.log("같음");
+            }
+
+            setSwitchModalNote(false);
           }
         }}
       >
-        <div className="modal"></div>
+        <div className="container-modal-note">
+          <div className="modal-note">
+            <div className="container-modal-note-subject">
+              <div
+                className="modal-note-subject"
+                contentEditable="true"
+                suppressContentEditableWarning="true"
+                onBlur={(e) => {
+                  const text = e.target.textContent;
+
+                  const copy = { ...modalNoteData };
+
+                  copy.title = text;
+
+                  console.log(copy.title);
+
+                  setModalNoteData(copy);
+                }}
+              >
+                {modalNoteData.title}
+              </div>
+            </div>
+
+            <div className="container-modal-note-content">
+              <div
+                className="modal-note-content"
+                contentEditable="true"
+                suppressContentEditableWarning="true"
+              >
+                {modalNoteData.content}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
